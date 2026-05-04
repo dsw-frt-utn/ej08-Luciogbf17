7deef75e27513b9041773f9678dc10bb638bd344
Luciogbf17 <gbflucio@gmail.com>
2026-05-03 21:09:46 -0300

    Problema 1

--- Dsw2026Ej8/Problema 1/ProductHelper1.cs
+++ Dsw2026Ej8/Problema 1/ProductHelper1.cs
@@ -5,7 +5,7 @@ public partial class ProductHelper
   public String ObtenerEtiquetaProducto(long code,string d, decimal price)
   {
       var description= DescripcionAMayuscula(d);
-      var etiqueta= $"{code},{description},-${price}";
+      var etiqueta= $"{code} - {description}-${price}";
 
       return etiqueta;
   }    
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Dsw2026Ej8
+﻿using Dsw2026Ej8.Domain;
+
+namespace Dsw2026Ej8
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        { Console.WriteLine("Hello, World!");
+            var problema1 = new ProductHelper().ObtenerEtiquetaProducto(123, "pastafrola", 1500);
+            Console.WriteLine(problema1);
+
+            var problema2 = new Problema2().CrearResumenVenta(124, "Papas lays", 3, 2000);
+            Console.WriteLine(problema2);
+
+            var p1 = new Product("bonobon");
+            var problema3 = new Problema3().CompararCopias(10, p1);
+            Console.WriteLine(problema3);
+            
+            
+            var problema4 = new Problema4().CalcularPromedio(null, null, null);
+            Console.WriteLine(problema4);
+            
+            
+            
+            
+            
+            
+            
+            
         }
     }
 }
